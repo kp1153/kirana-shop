7bd9ffd3f47616fd9c20b940f943d8002d2c8fe9
kp1153 <kamta_tiwari@yahoo.com>
2026-04-04 20:48:41 +0530

    force deploy

---
+++ electron/main.js
@@ -0,0 +1,15 @@
+const { app, BrowserWindow } = require("electron");
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1280,
+    height: 800,
+    icon: __dirname + "/../public/icon.ico",
+    webPreferences: { nodeIntegration: false },
+  });
+  win.loadURL("https://ration-pro.vercel.app");
+  win.setMenuBarVisibility(false);
+}
+
+app.whenReady().then(createWindow);
+app.on("window-all-closed", () => app.quit());
